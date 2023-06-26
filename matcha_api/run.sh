75e288bba7aca12aa6727588e52ab22e0a947f3c
pharick <artemforkunov@gmail.com>
2023-06-26 13:46:20 +0700

    try migrate until success

--- matcha_api/run.sh
+++ matcha_api/run.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
-migrate -database postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@db:5432/matcha?sslmode=disable -path migrations up
+while !(migrate -database postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@db:5432/matcha?sslmode=disable -path migrations up)
+do
+        echo "Migration is not successful. Trying again..."
+done
 app
