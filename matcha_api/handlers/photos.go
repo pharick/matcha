76b3c51333efcf96cffe2356a609f5c6dc877397
pharick <artemforkunov@gmail.com>
2023-06-28 11:58:43 +0700

    save photos to db

--- matcha_api/handlers/photos.go
+++ matcha_api/handlers/photos.go
@@ -1,17 +1,27 @@
 package handlers
 
 import (
-	"log"
+	"matcha_api/errors"
 	"matcha_api/lib"
+	"matcha_api/models"
 	"net/http"
+
+	"goji.io/pat"
 )
 
 func UploadPhoto(env *Env, w http.ResponseWriter, r *http.Request) (any, error) {
-	// username := pat.Param(r, "username")
+	username := pat.Param(r, "username")
+	user := r.Context().Value(ContextKey("User")).(models.User)
+	if username != user.Username {
+		return nil, errors.HttpError{Status: 403, Body: nil}
+	}
 	url, err := lib.UploadFile(r, "photo", []string{"image/jpeg", "image/png"}, "photos")
 	if err != nil {
 		return nil, err
 	}
-	log.Println(url)
-	return nil, nil
+	photo, err := env.Photos.Create(user.Id, url)
+	if err != nil {
+		return nil, err
+	}
+	return photo, nil
 }
