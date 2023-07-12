46508582c406d36407d35943b0eaa4cb31599135
pharick <artemforkunov@gmail.com>
2023-07-12 19:10:10 +0700

    tabs test

---
+++ matcha_front/src/app/page.tsx
@@ -0,0 +1,35 @@
+'use client';
+import { NextPage } from 'next';
+import { FC, PropsWithChildren, Children, useState } from 'react';
+
+interface TabsProps extends PropsWithChildren {
+  captions: string[];
+}
+
+const Tabs: FC<TabsProps> = ({ children, captions }) => {
+  const [index, setIndex] = useState(0);
+  return (
+    <div>
+      <ul>
+        {captions.map((caption, i) => (
+          <li key={i}>
+            <button onClick={() => setIndex(i)}>{caption}</button>
+          </li>
+        ))}
+      </ul>
+      {Children.map(children, (child, i) => (i == index ? child : <></>))}
+    </div>
+  );
+};
+
+const TestPage: NextPage = () => {
+  return (
+    <Tabs captions={['one', 'two', 'three']}>
+      <div>1</div>
+      <div>2</div>
+      <div>3</div>
+    </Tabs>
+  );
+};
+
+export default TestPage;
