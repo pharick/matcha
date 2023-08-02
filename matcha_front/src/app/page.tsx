2cf884a1e8bbcb0b60321b7bcf6b2c7bd5af688f
Milya <8milia01@mail.ru>
2023-08-02 19:37:39 +0700

    main page

--- matcha_front/src/app/page.tsx
+++ matcha_front/src/app/page.tsx
@@ -3,15 +3,17 @@ import { redirect } from 'next/navigation';
 
 import Header from '@/components/header/Header';
 import { getCurrentUser } from '@/api/auth';
+import Matcha from '@/components/Matcha';
 
 const IndexPage: NextPage = async () => {
   const user = await getCurrentUser();
   if (!user) redirect('/login');
 
   return (
-    <>
+    <div className="flex min-h-screen flex-col">
       <Header user={user} />
-    </>
+      <Matcha />
+    </div>
   );
 };
 
