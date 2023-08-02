e50ad51c020dd5379bde188ac91f1e2625313ff3
pharick <artemforkunov@gmail.com>
2023-08-02 18:19:16 +0700

    fix background

--- matcha_front/src/app/layout.tsx
+++ matcha_front/src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={montserrat.className}>
-      <body className="mx-10 bg-no-repeat bg-cover">{children}</body>
+      <body className="mx-10 min-h-screen">{children}</body>
     </html>
   );
 }
