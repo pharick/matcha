7b0f411437570094a592feb52c84d47c4f349d76
pharick <artemforkunov@gmail.com>
2023-08-14 15:29:48 +0700

    fix linter

--- matcha_front/src/components/MatchSpinner.tsx
+++ matcha_front/src/components/MatchSpinner.tsx
@@ -1,5 +1,5 @@
 'use client';
-import Image from 'next/image';
+import Image, { StaticImageData } from 'next/image';
 import { useState } from 'react';
 
 import Match from '@/images/Match.svg';
@@ -13,7 +13,12 @@ const MatchSpinner = () => {
         className="absolute left-0 top-0 z-40 flex h-full w-full items-center justify-center bg-neutral/50"
         onClick={() => isVisible(false)}
       >
-        <Image src={Match} alt="match" className="h-full" width={500} />
+        <Image
+          src={Match as StaticImageData}
+          alt="match"
+          className="h-full"
+          width={500}
+        />
       </div>
     );
 };
