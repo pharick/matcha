c27d5c884ea74b3b17a1d322a8b3432b95677e21
pharick <artemforkunov@gmail.com>
2023-08-30 19:39:17 +0700

    chat message table migration

--- matcha_api/migrations/000001_create_users_table.up.sql
+++ matcha_api/migrations/000001_create_users_table.up.sql
@@ -21,8 +21,8 @@ CREATE TABLE IF NOT EXISTS users (
 CREATE OR REPLACE FUNCTION update_modified_column() 
 RETURNS TRIGGER AS $$
 BEGIN
-    NEW.updated_at = now();
-    RETURN NEW;
+        NEW.updated_at = now();
+        RETURN NEW;
 END;
 $$ language 'plpgsql';
 CREATE TRIGGER update_user_modtime BEFORE UPDATE ON users FOR EACH ROW EXECUTE PROCEDURE update_modified_column();
---
+++ matcha_api/migrations/000010_create_chat_messages_table.up.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS chat_messages (
+        id SERIAL PRIMARY KEY,
+        from_user_id INTEGER NOT NULL REFERENCES users(id),
+        to_user_id INTEGER NOT NULL REFERENCES users(id),
+        text TEXT NOT NULL
+);
