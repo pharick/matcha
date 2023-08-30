eb2306276d3091f34db1bce92840c5979d44d61c
Milya <8milia01@mail.ru>
2023-08-30 19:14:29 +0700

    messenger is almost done

--- matcha_front/src/app/(app)/chat/[username]/components/Chat.tsx
+++ matcha_front/src/app/(app)/chat/[username]/components/Chat.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import { Field, Form, Formik } from 'formik';
 import { useEffect } from 'react';
 import useWebSocket from 'react-use-websocket';
@@ -14,6 +14,8 @@ interface NewChatMesageValues {
 }
 
 const Chat: FC<ChatProps> = ({ user }) => {
+  const [messageFrom, setMessage] = useState<ChatMessage[]>([]);
+
   const { lastJsonMessage, sendJsonMessage } = useWebSocket(
     `${process.env.NEXT_PUBLIC_WS_BASE_URL}/api/ws/chat/${user.username}/`
   );
@@ -22,6 +24,7 @@ const Chat: FC<ChatProps> = ({ user }) => {
     if (lastJsonMessage == null) return;
     const message = lastJsonMessage as ChatMessage;
     console.log(message);
+    setMessage((m) => [message, ...m]);
   }, [lastJsonMessage]);
 
   const initialValues: NewChatMesageValues = {
@@ -37,55 +40,35 @@ const Chat: FC<ChatProps> = ({ user }) => {
   };
 
   return (
-    <div className="absolute bottom-0 left-0 right-0 top-0 flex flex-col bg-white">
-      <ul className="flex-1 overflow-y-auto">
-        <li>Message</li>
-        <li>Message</li>
-        <li>Message</li>
-        <li>Message</li>
-        <li>Message</li>
-        <li>Message</li>
-        <li>Message</li>
-        <li>Message</li>
-        <li>Message</li>
-        <li>Message</li>
-        <li>Message</li>
-        <li>Message</li>
-        <li>Message</li>
-        <li>Message</li>
-        <li>Message</li>
-        <li>Message</li>
-        <li>Message</li>
-        <li>Message</li>
-        <li>Message</li>
-        <li>Message</li>
-        <li>Message</li>
-        <li>Message</li>
-        <li>Message</li>
-        <li>Message</li>
-        <li>Message</li>
-        <li>Message</li>
-        <li>Message</li>
-        <li>Message</li>
-        <li>Message</li>
-        <li>Message</li>
-        <li>Message</li>
-        <li>Message</li>
-        <li>Message</li>
-        <li>Message</li>
-        <li>Message</li>
-        <li>Message</li>
-      </ul>
+    <div className="absolute bottom-0 left-0 right-0 top-0 flex flex-row overflow-hidden rounded-lg bg-green-5/50 text-right">
+      <div className="w-[400px] bg-neutral/30"></div>
+      <div className="relative flex flex-1 flex-col px-2">
+        <ul className="flex-1 overflow-y-auto">
+          {messageFrom.map((m, i) => (
+            <li
+              key={i}
+              className="m-2 ml-auto w-fit rounded-lg bg-neutral/50 p-3"
+            >
+              {m.text}
+            </li>
+          ))}
+        </ul>
 
-      <div className="bg-green-1 p-2">
-        <Formik initialValues={initialValues} onSubmit={sendMessage}>
-          {() => (
-            <Form>
-              <Field type="text" name="message" placeholder="Message" />
-              <button type="submit">Submit</button>
-            </Form>
-          )}
-        </Formik>
+        <div className="w-full rounded-lg bg-neutral/30 p-2">
+          <Formik initialValues={initialValues} onSubmit={sendMessage}>
+            {() => (
+              <Form className="flex flex-row">
+                <Field
+                  className=" flex-1 bg-transparent"
+                  type="text"
+                  name="message"
+                  placeholder="Message"
+                />
+                <button type="submit">Submit</button>
+              </Form>
+            )}
+          </Formik>
+        </div>
       </div>
     </div>
   );
