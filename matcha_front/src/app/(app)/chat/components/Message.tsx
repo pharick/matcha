905fe8942667a2ff4557e45ad3c935a46dcb2221
pharick <artemforkunov@gmail.com>
2023-09-04 14:39:38 +0700

    refactoring

--- matcha_front/src/app/(app)/chat/components/Message.tsx
+++ matcha_front/src/app/(app)/chat/components/Message.tsx
@@ -19,13 +19,15 @@ const Message: FC<MessageProps> = ({
       <div className="mx-2 my-1 rounded-lg bg-neutral/50 p-3">
         <p className="break-words text-left">{children}</p>
       </div>
-      <div className="mx-3 flex items-center">
-        {!isCurrentUser && (
-          <p className="mr-1 text-xs text-gray-600">
-            {format(new Date(timestamp), 'dd.MM.yyyy H:mm')}
-          </p>
-        )}
-        <figure className="relative h-[30px] w-[30px] overflow-hidden rounded-full border-2 border-brown">
+      <div
+        className={`mx-3 flex items-center gap-1 ${
+          isCurrentUser && 'flex-row-reverse'
+        }`}
+      >
+        <p className="text-xs text-gray-600">
+          {format(new Date(timestamp), 'dd.MM.yyyy H:mm')}
+        </p>
+        <figure className="relative h-[30px] w-[30px] overflow-hidden rounded-full border border-brown">
           <Image
             src={
               avatar.startsWith('http')
@@ -38,11 +40,6 @@ const Message: FC<MessageProps> = ({
             alt="photo"
           />
         </figure>
-        {isCurrentUser && (
-          <p className="ml-1 text-xs text-gray-600">
-            {format(new Date(timestamp), 'dd.MM.yyyy H:mm')}
-          </p>
-        )}
       </div>
     </div>
   );
